f92d321d618b08d66599efccb8037cef24cddf5f
patrick hoehn <patrh_99@hotmail.com>
2019-10-04 02:10:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function theBeatlesPlay(musicians, instruments){
   for (var i=0; i < musicians.length; ++i){
     theyPlay.push(`${musicians[i]}`+ " plays " + `${instruments[i]}`)
   }
+  return theyPlay
 }
